ec425e618fa6adf49c26c0f4f4bff4396af907e2
asyam <asyam@LAPTOP-IBEUNTHH.localdomain>
2026-02-10 13:51:14 +0700

    Update contracts from RetroPick monorepo

--- packages/contracts/src/utils/Errors.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
-
-library Errors {
-    error Unauthorized();
-    error InvalidAddress();
-    error InvalidAmount();
-    error InvalidConfidence();
-}
